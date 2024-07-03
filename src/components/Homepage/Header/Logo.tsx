721892ad85cca2825c8e6be3769741eb2aa0f3f5
dominicpam89 <dominicpam89@gmail.com>
2024-07-03 16:06:29 +0700

    remove logo image

--- src/components/Homepage/Header/Logo.tsx
+++ src/components/Homepage/Header/Logo.tsx
@@ -1,63 +1,47 @@
-'use client';
-import { useState } from 'react';
+"use client";
+import { useState } from "react";
 
 interface Props {
-	width?: 'w-1/4' | 'w-2/4' | 'w-3/4';
+	width?: "w-1/4" | "w-2/4" | "w-3/4";
 }
 
 const useStyles = (hover: boolean) => {
-	const transitionClass = 'transition-all ease-in-out duration-300';
-	const logoImageClass = 'h-8';
+	const transitionClass = "transition-all ease-in-out duration-300";
+	const logoImageClass = "h-8";
 
 	const classesData = {
 		logoTextPhi: `${
-			hover
-				? 'text-secondary-dark-color'
-				: 'text-primary-main-color'
+			hover ? "text-secondary-dark-color" : "text-primary-main-color"
 		} ${transitionClass}`,
 		logoTextFinance: `${
-			hover
-				? 'text-primary-main-color'
-				: 'text-secondary-dark-color'
+			hover ? "text-primary-main-color" : "text-secondary-dark-color"
 		} ${transitionClass}`,
-		logoImage: `${logoImageClass} ${hover ? 'animate-cool-spin' : ''}`,
+		logoImage: `${logoImageClass} ${hover ? "animate-cool-spin" : ""}`,
 	};
 
 	return classesData;
 };
 
-export default function NavLogo({ width = 'w-1/4' }: Props) {
+export default function NavLogo({ width = "w-1/4" }: Props) {
 	const [hover, setHover] = useState(false);
 	const { logoTextFinance, logoTextPhi, logoImage } = useStyles(hover);
 	return (
 		<div className={`${width}`}>
 			<a
-				href="https://phifinance.id/"
+				href="https://dormate.co.id"
 				className="flex items-center space-x-2"
 				onMouseEnter={() => setHover(true)}
 				onMouseLeave={() => setHover(false)}
 			>
-				<img
-					aria-label="logo-image"
-					src="/logo-main.png"
-					className={logoImage}
-					alt="Flowbite Logo"
-				/>
 				<span
 					aria-label="logo-text"
 					className="self-center text-lg font-semibold whitespace-nowrap"
 				>
-					<span
-						aria-label="logo-text-phi"
-						className={logoTextPhi}
-					>
-						Phi
+					<span aria-label="logo-text-phi" className={logoTextPhi}>
+						Dorm
 					</span>
-					<span
-						aria-label="logo-text-finance"
-						className={logoTextFinance}
-					>
-						Finance
+					<span aria-label="logo-text-finance" className={logoTextFinance}>
+						Mate
 					</span>
 				</span>
 			</a>
--- src/components/Homepage/Header/NavList.tsx
+++ src/components/Homepage/Header/NavList.tsx
@@ -15,7 +15,7 @@ interface PropsLinkComp {
 const useStyles = (hover: boolean) => {
 	const baseClasses = {
 		link: "relative flex gap-1 items-center transition-all ease-in-out duration-300",
-		line: "transition-all ease-in duration-300 absolute top-[80%] left-1/2 -translate-x-1/4 rounded-full bg-primary-main-color",
+		line: "transition-all ease-in duration-300 absolute top-[90%] left-1/2 -translate-x-1/4 rounded-full bg-primary-main-color",
 	};
 
 	return {
@@ -24,19 +24,17 @@ const useStyles = (hover: boolean) => {
 		}`,
 		linkStyleActive: `${baseClasses.link} text-primary-main-color transform scale-[110%]`,
 		lineStyle: `${baseClasses.line} ${hover ? "w-1/2 h-[2px]" : "w-0 h-0"}`,
-		lineStyleActive: `${baseClasses.line}`,
 	};
 };
 
 const LinkComp = ({ item, active }: PropsLinkComp) => {
 	const [hover, setHover] = useState(false);
-	const { linkStyle, linkStyleActive, lineStyle, lineStyleActive } =
-		useStyles(hover);
+	const { linkStyle, linkStyleActive, lineStyle } = useStyles(hover);
 	return (
 		<Link
 			aria-label="nav-item"
 			href={item.href}
-			className={active ? linkStyleActive : linkStyle}
+			className={!active ? linkStyle : linkStyleActive}
 			onMouseEnter={() => setHover(true)}
 			onMouseLeave={() => setHover(false)}
 		>
@@ -46,10 +44,7 @@ const LinkComp = ({ item, active }: PropsLinkComp) => {
 			<span aria-label="text" className="text-[0.9rem]">
 				{item.text}
 			</span>
-			<div
-				aria-label="underline"
-				className={active ? lineStyleActive : lineStyle}
-			></div>
+			<div aria-label="underline" className={lineStyle}></div>
 		</Link>
 	);
 };
