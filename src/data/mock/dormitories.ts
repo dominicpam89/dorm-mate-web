29ffa8c17673427bb95e42c49cb917e6cdf18e04
dominicpam89 <dominicpam89@gmail.com>
2024-07-03 16:28:39 +0700

    added dormitories mock data

---
+++ src/data/mock/dormitories.ts
@@ -0,0 +1,10 @@
+export const dormitories = [
+	{ id: 1, name: "Dormitory A", location: "Location A" },
+	{ id: 2, name: "Dormitory B", location: "Location B" },
+	{ id: 3, name: "Dormitory C", location: "Location C" },
+	{ id: 4, name: "Dormitory D", location: "Location D" },
+	{ id: 5, name: "Dormitory E", location: "Location E" },
+	{ id: 6, name: "Dormitory F", location: "Location F" },
+];
+
+export type TDormitory = (typeof dormitories)[0];
